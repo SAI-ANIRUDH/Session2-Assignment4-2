c2f78ccc7131f285f2e0d8dabfd421f06c4a1b2b
SAI-ANIRUDH <SAI-ANIRUDH@users.noreply.github.com>
2017-07-19 12:04:37 +0530

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,36 +1,38 @@
-import java.util.Scanner;
-public class Student {
+package assignment2;
+import java.util.Scanner; // Importing Scanner class from Util Package //
+public class Student // Declaring class Student //
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 int P,C,M;
-	        int i;
-	        float sum=0, avg;
-	        Scanner scan = new Scanner(System.in);
-			System.out.print("Enter Marks Obtained in Physics  ");
-	        P=scan.nextInt();
+		 int P,C,M; // Declaring marks Physics P, Chemistry C, Maths M //
+	        int i; // Declaring iterator i //
+	        float sum=0, avg; // Declaring variables Sum and Avg //
+	        Scanner scan = new Scanner(System.in); // Intialising user input values using scanner class//
+			System.out.print("Enter Marks Obtained in Physics  "); 
+	        P=scan.nextInt(); // Taking input for physics marks from user //
 	        System.out.print("Enter Marks Obtained in Maths ");
-	        M=scan.nextInt();
+	        M=scan.nextInt(); // Taking input for Maths marks from user //
 	        System.out.print("Enter Marks Obtained in Chemistry ");
-	       C=scan.nextInt();
+	       C=scan.nextInt(); // Taking input for Chemistry marks from user //
 	        for(i=0; i<3;i++)
 	        {
 	            
-	            sum = sum +P+C+M;
+	            sum = sum +P+C+M; // Total of marks P, C, M //
 	        }
 			
-	        avg = sum/5;
+	        avg = sum/5; // Taking Average //
 			
 	        System.out.print("Your Grade is ");
 	        if(avg>70)
 	        {
-	            System.out.print("A");
+	            System.out.print("A"); // If avg of marks greater than 70 , display A //
 	        }
-	        else if(avg>61 && avg<70)
+	        else if(avg>61 && avg<70) // If avg of marks greater than 61 and less than 70, display A //
 	        {
 	            System.out.print("B");
 	        }
-	        else if(avg<=60)
+	        else if(avg<=60) // If avg of marks less than 60 display C //
 	        {
 	            System.out.print("C");
 	        }
